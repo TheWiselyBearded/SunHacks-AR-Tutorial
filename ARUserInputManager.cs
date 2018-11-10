c480c1224e2850cc29d6f90673db14ef041590e1
Alireza Bahremand <abahrema@asu.edu>
2018-11-10 10:31:58 -0700

    Unity package for scene, and scripts for user interaction. Package contains imports for Particle Systems and ARFoundation. Additional setup can be found in READme.

---
+++ ARUserInputManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ARUserInputManager : MonoBehaviour {
+    public GameObject airPlane;
+    private GameObject lastPlaneReference;
+
+    /*
+     * Per frame, check user gaze from smartphone camera.
+     * If collision found with objects, check if objects are of interest.
+     * Store reference to prior planes to properly orientate virtual objects.
+     */ 
+    void Update() {
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        // If raycast hits any object.
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo)) {
+            // Check object collision by tag, alternatively check by name.
+            if (hitInfo.collider.gameObject.CompareTag("AR Plane") ||
+                (hitInfo.collider.gameObject.name.Equals("AR Feathered Plane"))) {
+                // User has moved PoV to a new plane, recalibrate rotation.
+                if (lastPlaneReference != hitInfo.collider.gameObject || lastPlaneReference == null) {
+                    lastPlaneReference = hitInfo.collider.gameObject;   // Assign reference to plane
+                    airPlane.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+                }
+                airPlane.transform.position = new Vector3(hitInfo.point.x,
+                                                      hitInfo.point.y,
+                                                      hitInfo.point.z);
+                // Rotate the AirPlane to hug the surface of the hologram.
+                CheckRotation();
+            }
+        }
+    }
+
+    /*
+     * Check user input to alter y axis rotation (aka yaw of plane)
+     * Alter changed in rotation based on section of screen touched.
+     */ 
+    public void CheckRotation() {
+        // In current frame, check for first user touch.
+        if (Input.GetTouch(0).phase == TouchPhase.Stationary) {
+            Vector3 currentRotation = airPlane.transform.localEulerAngles;
+            // If user is holding left side of screen
+            if (Input.GetTouch(0).position.x < (Screen.width / 2)) {
+               currentRotation.y += 5f;
+            } else {    // User is holding right side of the screen.
+                currentRotation.y -= 5f;
+            }
+            // Assign changed rotation based on user drag.
+            airPlane.transform.localEulerAngles = currentRotation;
+        }
+    }
+
+}
---
+++ DropCar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCar : MonoBehaviour {
+    public GameObject convertibleCarPrefab;
+    public GameObject carExplosionPrefab;
+    private GameObject airPlane;
+
+	/*
+	 * At beginning of lifecycle, grab reference to the airplane/user gaze 
+	 * object to perform acitons on it per frame in Update.
+	 */
+	void Start () {
+        airPlane = gameObject.GetComponent<ARUserInputManager>().airPlane;
+	}
+	
+    /*
+     * Per frame, check if user has tapped screen.
+     * Instantiate a car prefab, and particle system explosion.
+     * Have objects destroy themselves within 1 second.
+     */ 
+	void Update () {
+        // In current frame, check for first user touch.
+        if (Input.GetTouch(0).phase == TouchPhase.Began) {
+            // Instantiate prefab.
+            GameObject car = (GameObject)Instantiate(convertibleCarPrefab,
+                                                      airPlane.transform.position,
+                                                      airPlane.transform.localRotation);
+            car.AddComponent<BoxCollider>();    // Add collider for detecting collisions.
+            car.AddComponent<Rigidbody>();  // Add gravity so it falls
+            Destroy(car, 0.7f); // Destroy game object after 1.5 seconds.
+            // Instantiate particle system.
+            GameObject carExplosion = Instantiate(carExplosionPrefab, car.transform.position, car.transform.rotation);
+            carExplosion.GetComponent<ParticleSystem>().Play(); // Play explosion
+            Destroy(carExplosion, 0.8f);  // Delete explosion instance.
+        }
+	}
+}
